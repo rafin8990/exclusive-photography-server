c42c3797b0d1ac610d8edcc588111e04fed6a4d2
rafin8990 <rafin8990@gmail.com>
2022-11-08 03:26:32 +0600

    home page services added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-const app=express();
-const port=process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,16 +11,29 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`
-
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('reviewSite').collection('services');
+       app.get('/', async (req, res)=>{
+        const query={}
+        const cursor=serviceCollection.find(query);
+        const services=await cursor.limit(3).toArray();
+        res.send(services)
+       })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Photography websitee server is runnning')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on the port: ${port}`)
 });
