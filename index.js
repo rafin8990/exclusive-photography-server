e64303fae53c1983efc3cb022919cebd4c70f44e
rafin8990 <rafin8990@gmail.com>
2022-11-08 04:50:18 +0600

    service details by id added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -28,6 +28,13 @@ async function run() {
         const cursor=serviceCollection.find(query);
         const services=await cursor.toArray();
         res.send(services)
+       });
+
+       app.get('/services/:id', async (req, res)=>{
+        const id=req.params.id 
+        const query={_id: ObjectId(id)}
+        const result= await serviceCollection.findOne(query)
+        res.send(result)
        })
     }
     finally {
