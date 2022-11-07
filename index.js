d0edc23a71f27446661e7f71e0f592b1d5acc2e5
rafin8990 <rafin8990@gmail.com>
2022-11-08 04:11:51 +0600

    service page data loaded

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ async function run() {
         const cursor=serviceCollection.find(query);
         const services=await cursor.limit(3).toArray();
         res.send(services)
+       });
+
+       app.get('/services', async (req, res)=>{
+        const query={}
+        const cursor=serviceCollection.find(query);
+        const services=await cursor.toArray();
+        res.send(services)
        })
     }
     finally {
